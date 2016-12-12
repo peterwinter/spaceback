2ef84654608347109239082027945f6fd68bfd8a
Peter Winter <peterwinteriii@gmail.com>
2016-12-12 15:28:52 -0600

    update command names to reflect readme

--- spaceback.py
+++ spaceback.py
@@ -12,18 +12,32 @@ __status__ = 'prototype'
 
 @click.group()
 def main():
-    """primary command line function, called with options"""
+    """First paragraph.
+
+    This is a very long second paragraph and as you
+    can see wrapped very early in the source text
+    but will be rewrapped to the terminal width in
+    the final output.
+
+    \b
+    This is
+    a paragraph
+    without rewrapping.
+
+    And this is a paragraph
+    that will be rewrapped again.
+    """
     # if ~/.spaceback does not exist make it
     sb.create_spacedir()
 
 @click.command()
-def archive():
-    """saves active spacemacs configuration as new archive entry"""
+def save():
+    """saves active spacemacs configuration"""
     active_sm = sb.active_spacemacs()
     active_emc = sb.active_emacsconfig()
 
     backup_id = sb.current_id()
-    print('archive --> {bid}'.format(bid=backup_id))
+    print('saving --> {bid}'.format(bid=backup_id))
 
     bu_sm = sb.archived_spacemacs(backup_id)
     bu_em = sb.archived_emacsconfig(backup_id)
@@ -40,19 +54,21 @@ def archive():
 @click.command()
 # @click.argument('backup_id', optional=True)
 def show(): #backup_id):
+    """shows all available backups"""
     print('backups:')
     sb.show()
 
 
 # TODO: get it working
 @click.command()
-@click.argument('backup_id')
+@click.argument('backup_id', nargs=-1)
 def load(backup_id):
+    """activates a backup"""
     print('loading backup: {bid}'.format(bid=backup_id))
     sb.run_load(backup_id)
 
 
-main.add_command(archive)
+main.add_command(save)
 main.add_command(show)
 main.add_command(load)
 
